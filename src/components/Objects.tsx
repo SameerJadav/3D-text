2ab60a1ca835526d68156524e3387d04967a12b0
SameerJadav <sameerjadav010@gmail.com>
2023-05-04 20:11:01 +0530

    Optimization

--- src/components/Objects.tsx
+++ src/components/Objects.tsx
@@ -3,76 +3,74 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Text3D, Center } from "@react-three/drei";
 
 export function Cube() {
-  const meshNormalMaterial = <meshNormalMaterial />;
+  const meshNormalMaterial = <meshNormalMaterial wireframe />;
 
-  const donuts = Array.from({ length: 200 }, (_, i) => {
-    let randomSize = Math.random();
-    return (
-      <mesh
-        key={i}
-        position={[
-          (Math.random() - 0.5) * 12.5,
-          (Math.random() - 0.5) * 12.5,
-          (Math.random() - 0.5) * 12.5,
-        ]}
-        rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        scale={[randomSize, randomSize, randomSize]}
-      >
-        <torusBufferGeometry args={[0.3, 0.1, 20, 40]} />
-        {meshNormalMaterial}
-      </mesh>
-    );
-  });
+  const generateMeshes = (meshType: string, length: number) => {
+    return Array.from({ length }, (_, i) => {
+      let randomSize = Math.random();
+      return (
+        <mesh
+          key={i}
+          position={[
+            (Math.random() - 0.5) * 13,
+            (Math.random() - 0.5) * 13,
+            (Math.random() - 0.5) * 13,
+          ]}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+          scale={[randomSize, randomSize, randomSize]}
+        >
+          {(() => {
+            switch (meshType) {
+              case "torus":
+                return <torusBufferGeometry args={[0.3, 0.1, 20, 40]} />;
+              case "box":
+                return <boxBufferGeometry args={[0.3, 0.3, 0.3, 4, 4, 4]} />;
+              case "sphere":
+                return <sphereBufferGeometry args={[0.3, 12, 10]} />;
+              default:
+                return <boxBufferGeometry args={[0.3, 0.3, 0.3]} />;
+            }
+          })()}
+          {meshNormalMaterial}
+        </mesh>
+      );
+    });
+  };
 
-  const cubes = Array.from({ length: 200 }, (_, i) => {
-    let randomSize = Math.random();
-    return (
-      <mesh
-        key={i}
-        position={[
-          (Math.random() - 0.5) * 12.5,
-          (Math.random() - 0.5) * 12.5,
-          (Math.random() - 0.5) * 12.5,
-        ]}
-        rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        scale={[randomSize, randomSize, randomSize]}
-      >
-        <boxBufferGeometry args={[0.3, 0.3, 0.3]} />
-        {meshNormalMaterial}
-      </mesh>
-    );
-  });
+  const donuts = generateMeshes("torus", 110);
+  const cubes = generateMeshes("box", 110);
+  const sphere = generateMeshes("sphere", 110);
 
   return (
     <Canvas
-      camera={{ position: [0, 0, 3], fov: 75 }}
+      camera={{ position: [1, -1, 6], fov: 45 }}
       className="bg-neutral-950"
     >
       <OrbitControls
         makeDefault
-        enableZoom={false}
         minAzimuthAngle={-Math.PI / 2}
         maxAzimuthAngle={Math.PI / 2}
       />
       <Center>
         <Text3D
           font="/fonts/helvetiker_regular.typeface.json"
+          // letterSpacing={0.025}
           size={0.5}
           height={0.2}
           curveSegments={5}
           bevelEnabled
-          bevelThickness={0.03}
-          bevelSize={0.02}
+          bevelThickness={0.01}
+          bevelSize={0.01}
           bevelOffset={0}
           bevelSegments={5}
         >
-          Marcus Aurelius
-          {/* <meshNormalMaterial /> */}
-          {meshNormalMaterial}
+          Amor fati
+          <meshNormalMaterial />
         </Text3D>
       </Center>
       {donuts}
       {cubes}
+      {sphere}
     </Canvas>
   );
 }
